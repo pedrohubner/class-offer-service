29c23368dc448096120090d58aafee021cccf845
phubner <phubner@grupopanvel.com.br>
2023-11-18 19:32:10 -0300

    feat: adding class offers routes/

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const app = require('express')();
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 const PORT = 8080;
 
 app.use(bodyParser.json());
+
+mongoose.connect('mongodb://db:27017/class-offer', {
+    useNewUrlParser: true
+})
+    .then(result => {
+        console.log('MongoDB Conectado');
+    })
+    .catch(error => {
+        console.log(error);
+    });
 
 var route = require('./routes/admin-route')(app);
 var route = require('./routes/offer-route')(app);
--- routes/offer-route.js
+++ routes/offer-route.js
@@ -1,8 +1,29 @@
-// const service = require("../services/offer-service");
+const service = require("../services/offer-service");
 
-// module.exports = function (app) {
-//     app.get('/home', (req, res) => {
-//         let obj = service.getHome();
-//         res.status(200).send(obj);
-//     });
-// };
+module.exports = function (app) {
+    app.post('/class-offer', (req, res) => {
+        let obj = service.registerClassOffer(req);
+        res.status(201).send(obj);
+    });
+};
+
+module.exports = function (app) {
+    app.get('/class-offer/students', (req, res) => {
+        let obj = service.getClassesForStudents(req);
+        res.status(200).send(obj);
+    });
+};
+
+module.exports = function (app) {
+    app.get('/class-offer/teachers', (req, res) => {
+        let obj = service.getClassesForTeachers(req);
+        res.status(200).send(obj);
+    });
+};
+
+module.exports = function (app) {
+    app.patch('/class-offer', (req, res) => {
+        let obj = service.registerStudentOrPersonToClassOffer(req);
+        res.status(200).send(obj);
+    });
+};
